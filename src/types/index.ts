abf19bf4e2e05dc62e56be2bef6b5f12f531114c
kerkeniaziz <kerkeniaziz@gmail.com>
2025-05-09 09:51:55 +0100

    adding all code ref possible for the work

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Post {
+    id: string;
+    author: string;
+    title: string;
+    metaTitle: string;
+    metaDescription: string;
+    description: string;
+    status: string;
+    createdAt: string;
+    updatedAt: string;
+
+}
